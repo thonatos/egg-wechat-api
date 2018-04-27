3f160d9b1edea4f197f43dc5aafd9860c7ae6842
PowerDos <543541941@qq.com>
2018-04-27 20:18:01 +0800

    modify redis setup

--- test/travis/redis_setup.sh
+++ test/travis/redis_setup.sh
@@ -23,6 +23,6 @@ sudo chown redis:redis /var/lib/redis2
 
 echo "Starting Second Service..."
 
-sudo /etc/init.d/redis-server2 start
+/etc/init.d/redis-server2 start
 sleep 3
 echo "Finished setup of second redis server."

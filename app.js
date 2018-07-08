231fc38654ffdd13057a9fbed097bb42640c6aed
Suyi <thonatos.yang@gmail.com>
2018-07-08 19:47:46 +0800

    test: update test case

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const assert = require('assert');
 
 module.exports = app => {
-  const { appId, appSecret } = app.config.WechatApi || {};
+  const { appId, appSecret } = app.config.wechatApi || {};
   assert(appId && appSecret,
     '[egg-wechat-api] Must set `appId` and `appSecret` and in wechat-api\'s config');
 };
